8044ec14d6b8ceb3d9765c69a26a971194a5c8d1
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-07-02 17:17:11 +0200

    Working circle animation

---
+++ script.js
@@ -0,0 +1,14 @@
+const waitTime = 5500; // 5.5 seconds in milliseconds
+circle = document.getElementById('circle');
+
+function start() {
+    toggleCircle();
+    $('#text').text('Breathe in for 4 seconds');
+    setInterval(toggleCircle, waitTime); // Then every 5.5 seconds
+
+}
+
+function toggleCircle() {
+    circle.classList.toggle('open');
+    circle.classList.contains('open') ? $('#text').text('Breathe in for 4 seconds') : $('#text').text('Breathe out for 4 seconds');
+}
